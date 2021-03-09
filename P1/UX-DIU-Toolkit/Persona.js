8086a10faf548ee0eba1486fca08aaa1cc12a020
Pablo Cordero <pablocorderoromero@gmail.com>
2021-03-09 19:40:56 +0100

    P1: Añadidas personas

--- P1/UX-DIU-Toolkit/Persona.js
+++ P1/UX-DIU-Toolkit/Persona.js
@@ -17,7 +17,7 @@
 
 angular.module("angular", [])
 	.controller("controller", ["$scope", function($scope) { 
-        $scope.Grupo_ID ="DIU1.ABCDEF";
+        $scope.Grupo_ID ="DIU1.PJ";
         $scope.Curso ="2020/21";
         $scope.Github_ID ="https://github.com/mgea/UX-DIU-Toolkit";
         
@@ -33,36 +33,42 @@ angular.module("angular", [])
                 
                 
 				Id: 0,
-				Name: "Pedro",
-				Photo: "man.png",
-				Quote: "Carpe Diem",
-				Age: 32,
-				Occupation: "Comercial en empresa de automocion",
-				Family: "Con pareja desde hace 1 año",
-				Location: "Granada (Huetor Vega)",
-				Character: "Le gusta disfrutar del tiempo",
+				Name: "Juan Bolívar",
+				Photo: "hombre.png",
+				Quote: "Vivir hasta que duela",
+				Age: 28,
+				Occupation: "Funcionario de prisiones",
+				Family: "Con pareja desde hace 3 años",
+				Location: "Canarias (Las Palmas de Gran Canaria)",
+				Character: "Cuidar animales y el rock",
 				PersonalityTraits: [
 					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 4 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 1 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 3 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 5 }
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 4 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 5 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 4 }
 				], 
-				Goals: ["Disfrutar del tiempo libre, viajar", "Cambiar a un trabajo más estable"],
-				Frustrations: ["le gusta la tecnología, pero siempre 'llama a un amigo' para resolver problemas", "Le gustaría tener más tiempo libre y leer más"],
-				Bio: "Es de Modril y vino a Granada para estudiar administración de empresas, pero no ha tenido grandes oportunidades de trabajo. LLeva 2 años contratado en la empresa actual. Aqui ha hecho buenos amigo en el trabajo y normalmente ser reunen para fiestas y a veces organizan viajes",
+				Goals: ["Pasar más tiempo con su pareja Elvira",
+						"Conocer más acerca del mundo animal",
+						"Explorar nuevas culturas, tanto musicales como sociales"],
+				Frustrations: [
+					"No saber como llevar una relación a distancia",
+				 	"Pasar mas tiempo con sus seres queridos",
+					"No conseguir avances en su aprendizaje musical (guitarra)"
+				],
+				Bio: "Juan es una persona nacida y criada en la provincia de Granada. Pertenece a una familia adinerada, donde su padre es representante de menaje de hostelería y diversos. Su madre es veterinaria aunque se encuentre ya jubilada. Juan ha estudiado un grado superior de auxiliar administrativo y comenzó la carrera de Administración y Dirección de Empresas, la cual no finalizó debido a una gran desilusión sobre lo esperado en la carrera. Tras esto, decidió realizar las oposiciones a funcionario de prisiones en las cuales obtuvo una plaza en las Islas Canarias, por lo que tuvo que mudarse allí. Además tiene una gran pasión por los animales, algo que su madre le inculcó desde su infancia, por lo que decidió pertenecer a una protectora de animales para poder ayudarlos en todo lo que este en su mano. Tiene una gran pasión por el rock y desde los 26 años comenzó a aprender a tocar la guitarra. Por último mantiene una relación de 3 años con Elvira que actualmente estudia y vive en Granada, por lo que su relación actualmente es a distancia desde hace 6 meses.",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 2 },
-					{ Name: "Movil", Value: 2 },
-					{ Name: "RRSS", Value: 3 },
+					{ Name: "TIC/Internet", Value: 3 },
+					{ Name: "Movil", Value: 4 },
+					{ Name: "RRSS", Value: 4 },
 					{ Name: "Software", Value: 2 }
 					
 				], 
-                Contextos: "LLeva un tiempo preocupado y quiere desconectar Le gustaría dar una sorpresa a sus amigos para las vacaciones",  
+                Contextos: "Le gustaría darle una sorpresa a su pareja, a la vez que explorar mundo. Además, siempre ha querido adentrarse más en el mundo animal, visitanto sus hábitats naturales.",  
 				PreferredChannels: [
-					{ Name: "Publicidad Tradicional", Value: 5 },
-					{ Name: "Online & Social Media", Value: 3 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
+					{ Name: "Publicidad Tradicional", Value: 2 },
+					{ Name: "Online & Social Media", Value: 5 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 4 }
 				]
 			},
 			{	
@@ -74,36 +80,40 @@ angular.module("angular", [])
                 
                 
 				Id: 1,
-				Name: "Monica Suarez",
-				Photo: "woman.png",
-				Quote: "A quotation that captures the essence of this person's personality",
-				Age: 17,
-				Occupation: "Searching for a cure for the Empress",
-				Family: "No parents, only family are the people who raised him.",
-				Location: "The Grassy Plains of Fantasia",
-				Character: "Strong, reliable and fearless.",
+				Name: "Aliona Ekaterina",
+				Photo: "mujer.png",
+				Quote: "Если вы можете мечтать об этом, вы можете это получить (Si lo puedes soñar, lo puedes lograr)",
+				Age: 38,
+				Occupation: "Costurera",
+				Family: "Soltera",
+				Location: "Stára Ládoga, Rusia",
+				Character: "No tiene ataduras que la aten a su pueblo y tener nuevas experiencias.",
 				PersonalityTraits: [
-					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
-					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
-					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
-					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 1 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 2 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 4 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 3 }
 				], 
-				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
-				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
-				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Goals: ["Conocer una persona con la que crear una familia",
+						"Conocer lugares nuevos", 
+						"Aprovechar el tiempo como no lo ha hecho hasta ahora"],
+				Frustrations: ["Debido a la pérdida de su única familia, se siente sola",
+							   "Tiene la creencia de haber desaprovechado parte de su vida",
+							   "Nunca ha tenido un objetivo claro"],
+				Bio: "Aliona es una mujer de 38 años que reside actualmente en una pueblo cercano a Stáraya Ládoga(Rusia). Se ha criado en una ambiente muy familiar y protegido, lo que ha produjo una excesiva protección y falta de metas y objetivos en la vida. Tuvo una relación formal a los 25 años, la cual no acabo consolidándose para formar una familia. Alrededor de un año Aliona falleció su madre, el único familiar vivó. Este suceso le aporto una gran herencia familiar, sin embargo le produjo un fuerte vacio existencial que decidió llenar eliminado todas sus ataduras con su pueblo, comenzando a explorar nuevos lugares, experiencias, pensamientos , etc",
 				Tech: [
-					{ Name: "TIC/Internet", Value: 5 },
+					{ Name: "TIC/Internet", Value: 2 },
 					{ Name: "Mobile", Value: 3 },
-					{ Name: "RRSS", Value: 3 },
-					{ Name: "Software", Value: 5 }
+					{ Name: "RRSS", Value: 2 },
+					{ Name: "Software", Value: 1 }
 					
 				], 
-                Contextos:   "The goals this user hopes to achieve." ,
+                Contextos: "Con el contexto de la herencia obtenida de su padre, le gustaría conocer otros lugares y culturas." ,
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
-					{ Name: "Online & Social Media", Value: 2 },
-					{ Name: "Recomendaciones & sugerencias", Value: 2 },
-					{ Name: "Persona confianza (amigos, boca a boca)", Value: 2 }
+					{ Name: "Online & Social Media", Value: 1 },
+					{ Name: "Recomendaciones & sugerencias", Value: 3 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 5 }
 				]
 			}
 		];
